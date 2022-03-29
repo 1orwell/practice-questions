fde06eff310a56bd65a5e10d1267445b01f58cd7
Elise Ratcliffe <elise@eliseratcliffe.co.uk>
2022-03-29 11:27:39 +0100

    Update caesar_cipher.py
    added other solutions with comments

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -7,5 +7,17 @@ def caeser(message, shift):
             new_orders[i] = val-26
     return list(map(chr, new_orders))
 
+def caeser2(message, shift):
+    #create string of all lowercase ascii characters from a to z
+    letters = string.ascii_lowercase
+    #e.g. letters[3:] = def...z
+    # e.g. letters[:3] = ab
+    # creates new string, e.g. def...zabc
+    mask = letters[shift:] + letters[:shift]
+    # matches each value in letters to the corresponding value in mask
+    trantab = str.maketrans(letters, mask)
+    # applies above translation to the message
+    return message.translate(trantab)
+
 # need to fix issue with spaces
 print(caeser("abcd xyz", 4))
